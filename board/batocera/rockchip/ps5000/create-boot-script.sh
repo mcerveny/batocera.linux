fbb27b06c40457cfdda46f4c8285a86c3bb65479
acmeplus <ctalon@gmail.com>
2021-09-21 14:31:57 +0000

    Fixed uboot compilation
    - Fixed uboot script to generate idbloader.img, trust.img, and uboot.img
    - WIP: need to pass parallel make -j flag with proper cpu count. Need to
      tidy up uboot compilation, maybe use uboot provided make.sh
    - Added PS5000 platform flags for libretro-mupen64plus-next compilation
    - WIP: libretro-flycast still broken, needs proper compilation flags

--- board/batocera/rockchip/ps5000/create-boot-script.sh
+++ board/batocera/rockchip/ps5000/create-boot-script.sh
@@ -6,7 +6,6 @@
 # BINARIES_DIR = images dir
 # TARGET_DIR = target dir
 # BATOCERA_BINARIES_DIR = batocera binaries sub directory
-# BATOCERA_TARGET_DIR = batocera target sub directory
 
 HOST_DIR=$1
 BOARD_DIR=$2
@@ -15,15 +14,14 @@ BINARIES_DIR=$4
 TARGET_DIR=$5
 BATOCERA_BINARIES_DIR=$6
 
-mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
 
-"${HOST_DIR}/bin/mkimage" -n rk3128 -T rksd -d "${BINARIES_DIR}/rk3128_ddr_300MHz_v2.12.bin" "${BINARIES_DIR}/idbloader.img" || exit 1
-#-A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/linux" || exit 1
-#"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/linux" || exit 1
-cp "${BINARIES_DIR}/uInitrd"                        "${BATOCERA_BINARIES_DIR}/boot/boot/uInitrd"                             || exit 1
-cp "${BINARIES_DIR}/rootfs.squashfs"                "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"                     || exit 1
+cp "${BINARIES_DIR}/zImage"             "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"          "${BATOCERA_BINARIES_DIR}/boot/boot/"                || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs"    "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 
-cp "${BINARIES_DIR}/rk3128-wx8.dtb"                 "${BATOCERA_BINARIES_DIR}/boot/boot/rk3128-wx8.dtb"                      || exit 1
-cp "${BOARD_DIR}/boot/boot.ini"                     "${BATOCERA_BINARIES_DIR}/boot/"                                         || exit 1
+cp "${BINARIES_DIR}/rk3128-wx8.dtb"     "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"    "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
 
 exit 0
